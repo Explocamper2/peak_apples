ead6f04318e0a1e21e59c69ebea2e18a690c31d0
flalala123456 <therealflalala123456@gmail.com>
2025-07-31 11:39:52 -0400

    added main menu

--- new-game-project/main_menu.gd
+++ new-game-project/main_menu.gd
@@ -1,4 +1,29 @@
 extends Node2D
 
 @onready var play_button: Button = $play_button
+@onready var credits_button: Button = $credits_button
+@onready var exit_button: Button = $exit_button
 @onready var title: Label = $title
+@onready var anim_player: AnimationPlayer = $AnimPlayer
+
+func fade_frame(state : bool) -> void:
+	if state == true: anim_player.play("FadeFrameIn")
+	else: anim_player.play("FadeFrameOut")
+	await anim_player.animation_finished
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(_delta) -> void:
+	if play_button.pressed:
+		fade_frame(false)
+		get_tree().change_scene_to_file("res://main.tscn")
+	elif credits_button.pressed:
+		fade_frame(false)
+		get_tree().change_scene_to_file("res://credits.tscn")
+	elif exit_button.pressed:
+		pass
+		# play really loud sound
+		
